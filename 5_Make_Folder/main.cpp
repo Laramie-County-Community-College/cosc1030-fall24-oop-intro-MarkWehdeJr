86c259c7ad7eb94422404c2ef18f9251b1522f7a
MarkWehdeJr <wehde98@gmail.com>
2025-11-12 20:08:36 +0000

    Homework Submission

--- 5_Make_Folder/main.cpp
+++ 5_Make_Folder/main.cpp
@@ -20,15 +20,23 @@ using namespace std;
 int main() {
    Triangle triangle1;
    Triangle triangle2;
-
-   // TODO: Read and set base and height for triangle1 (use SetBase() and SetHeight())
-      
-   // TODO: Read and set base and height for triangle2 (use SetBase() and SetHeight())
-       
+   double base, height;
+   // DONE: Read and set base and height for triangle1 (use SetBase() and SetHeight())
+   cin >> base >> height;
+   triangle1.SetBase(base);
+   triangle1.SetHeight(height);
+   // DONE: Read and set base and height for triangle2 (use SetBase() and SetHeight())
+   cin >> base >> height;
+   triangle2.SetBase(base);
+   triangle2.SetHeight(height);    
    cout << "Triangle with smaller area:" << endl;
    
    // TODO: Determine smaller triangle (use GetArea())  
    //       and output smaller triangle's info (use PrintInfo())
-   
+      if (triangle1.GetArea() < triangle2.GetArea()) {
+      triangle1.PrintInfo();
+   } else {
+      triangle2.PrintInfo();
+   }
    return 0;
 }
